1db93a8ec984b02ebeadf0d70b87c4b891651bb9
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-06-13 09:49:51 +0300

    commit tree cb41553a4e73fdcef46f48a1f53d408b820d4c2d

--- index.js
+++ index.js
@@ -28,21 +28,24 @@ const nestedMuppet = {
 // Strings
 
 // 1. Use destructuring to assign appropriate variables based on the sounds animals make.
-
+let [moo,neigh,baa,oink,cluck]=farmAnimals.split(" ")
 // 2. Bolt the horse wandered off, so just give us four animals, and let's name them bessie, dolly, babe, and little.
-
+let [bessie,dolly,babe,little]=["cow","sheep",'pig','chicken']
 // 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of blackAndWhite, black, and pink.
-
+let [r,o,y,g,b,i,v]=colors
 // Arrays
 
 // 4. Use destructuring to assign appropriate variables using the color names.
-
+let [blackAndWhite,black,pink]=["cow","sheep","pig"]
 // 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
 
 // 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg. 
-
+let [indg]=["indigo"]
 // Objects
 
 // 7. Use destructuring to assign all variables using the keys as the variable names
-
+let {muppetName,color,song,job,partner}=muppet
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
+let {nestedJob,nestedPartner}=nestedMuppet
+console.log(nestedJob)
+let {song2,song4}=nestedMuppet.theMuppetMovie
